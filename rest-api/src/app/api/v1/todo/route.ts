286a6f7c9554748871aa843b9beaebc12804a9b0
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-08-19 21:30:49 +0600

    add  task

--- rest-api/src/app/api/v1/todo/route.ts
+++ rest-api/src/app/api/v1/todo/route.ts
@@ -1,5 +1,5 @@
-import { tasks } from "@/app/model/tasks"
-import { NextResponse } from "next/server"
+import { isValidTask, Task, tasks } from "@/app/model/tasks"
+import { NextRequest, NextResponse } from "next/server"
 
 
 export const GET = async () => {
@@ -7,4 +7,28 @@ export const GET = async () => {
         tasks,
         { status: 200 }
     )
+}
+
+export const POST = async (req: Request) => {
+    try {
+        const body: any = await req.json()
+        const task: Task = (await body)
+
+        if (!isValidTask(task)) {
+            return NextResponse.json(
+                { "error": true, "message": "invalid request" },
+                { status: 400 }
+            )
+        }
+
+        tasks.push(task)
+        return NextResponse.json(tasks, { status: 200 })
+
+    } catch (error) {
+        return NextResponse.json(
+            { "error": true, },
+            { status: 500 }
+        )
+    }
+
 }
