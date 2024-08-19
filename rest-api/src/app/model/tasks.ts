e9cc69580632731b26efd68aacd83e64d32a6c45
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-08-19 21:46:25 +0600

    add task

--- rest-api/src/app/model/tasks.ts
+++ rest-api/src/app/model/tasks.ts
@@ -4,11 +4,18 @@ export interface Task {
     body : string
 } 
 
-export const totalTasks : number = 0;
+export let totalTasks : number = 0;
+
 
 export const tasks : Task[] = [];
 
 
 export function isValidTask(obj : any) {
     return typeof obj==='object' && typeof obj.title==='string' && typeof obj.body==='string'
+}
+
+export function addTask(task : Task) : Task {
+    tasks.push({...task, id : totalTasks + 1})
+    totalTasks ++
+    return tasks[totalTasks - 1]
 }
