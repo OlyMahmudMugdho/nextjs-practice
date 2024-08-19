b357438299813f1739f63ef46b6f497ad861a643
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-08-19 22:19:38 +0600

    edit task

--- rest-api/src/app/api/v1/todo/[id]/route.ts
+++ rest-api/src/app/api/v1/todo/[id]/route.ts
@@ -1,10 +1,11 @@
-import { tasks } from "@/app/model/tasks";
+import { Task, tasks, totalTasks } from "@/app/model/tasks";
 import { request } from "http";
 import { NextResponse } from "next/server";
 
 
 export const GET = async (request : Request, context : { params : any }) => {
     const taskId = context.params.id;
+
     try {
         const task = tasks.filter(item => item.id===parseInt(taskId))
         return NextResponse.json(task[0] || {})
@@ -15,6 +16,35 @@ export const GET = async (request : Request, context : { params : any }) => {
                 "error" : true,
                 "message" : "internal server error"
             },
+            { status : 500 }
+        )
+    }
+}
+
+
+export const PUT = async (request : Request, context : { params : any }) => {
+    const taskId = context.params.id;
+
+    try {
+
+        if (taskId > totalTasks || taskId <= 0) {
+            return NextResponse.json(
+                { "error" : true, "message" : "invalid task id" },
+                { status : 400 }
+            )
+        }
+
+        const body : any  = await request.json()
+        const task : Task = (await body);
+
+        tasks[taskId] = task;
+
+        return NextResponse.json(tasks[taskId])
+        
+    } catch (error) {
+        console.error(error)
+        return NextResponse.json(
+            { "error" : true, "message" : "internal server error" },
             { status : 500 }
         )
     }
